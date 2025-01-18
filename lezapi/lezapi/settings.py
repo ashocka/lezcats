fa9d18321da1587dcacd7ecafd674701f326f8df
Erika Pogorelc <erika.pogorelc@xapo.com>
2025-01-18 16:02:23 +0100

    [main] Use environment variables for sensitive settings

--- lezapi/lezapi/settings.py
+++ lezapi/lezapi/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-nuc4c69vvj4ukl11)+vgt=gq$d*5af$k0bug)#a(u$eyga&l1r')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nuc4c69vvj4ukl11)+vgt=gq$d*5af$k0bug)#a(u$eyga&l1r"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
