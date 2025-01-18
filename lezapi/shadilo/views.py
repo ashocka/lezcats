e1450df4a4a575ef40c2da30a94313bcc30f47bd
Erika Pogorelc <erika.pogorelc@xapo.com>
2025-01-18 16:14:21 +0100

    [main] Refactor import and update index view response.

--- lezapi/shadilo/views.py
+++ lezapi/shadilo/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from lezapi.shadilo.shadilo import get_facebook_events
+from .shadilo import get_facebook_events
 
 
 # Function-Based View
 def index(request):
     events = get_facebook_events('page_id')
-    return HttpResponse("Hello, world. You're at the index of your new Django app.")
+    return HttpResponse("Hello, world. You're at the index of your new Django app. With events" + str(events))
 
